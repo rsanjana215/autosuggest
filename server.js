93d7e34a3ae7a441b8171ca04cfb8030e4d08971
rsanjana215 <rsanjana3028@gmail.com>
2026-07-03 12:03:16 +0530

    Add server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+{
+
+}
+
+const express = require('express');
+
+const app = express()
+
+const PORT = 3002;
+
+// web server : 
+app.use(express.static("frontend"));
+
+app.listen(PORT, function() {
+    console.log("sucessfully running at http://localhost:"+PORT);
+})
